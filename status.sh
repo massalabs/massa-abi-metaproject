8d8f2272525cd3b0998039d13ebb6e5b15e8c702
Jean-François Morcillo <jfm@massa.net>
2023-06-09 21:31:46 +0200

    Point submodules to common dev branches
    Signed-off-by: Jean-François Morcillo <jfm@massa.net>

--- status.sh
+++ status.sh
@@ -1,4 +1,4 @@
-for d in massa-proto-rs massa-proto-as massa-rust-sc-* as_abi_protobuf massa-sc-runtime ; do
+for d in massa-proto-rs massa-proto-as massa-rust-sc-* as_abi_protobuf massa-sc-runtime; do
 	cd $d
 	echo ""
 	echo "***" $(pwd)
@@ -8,3 +8,15 @@ for d in massa-proto-rs massa-proto-as massa-rust-sc-* as_abi_protobuf massa-sc-
 	git status -s
 	cd ..
 done
+
+check_proto_rev() {
+	# check that every modules use the same revision of massa-proto
+	PROTO_REV=$(git submodule foreach --recursive --quiet 'echo $sm_path $(git rev-parse HEAD)' | grep "massa-proto " | sort -u)
+	PROTO_REV_CHECK=$(echo $PROTO_REV | wc -l)
+	if [ $PROTO_REV_CHECK -ne "1" ]; then
+		echo $PROTO_REV
+		echo "are diverging"
+	else
+		echo "ok"
+	fi
+}
